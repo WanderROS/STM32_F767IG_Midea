d599efc778bb5930b12eafba937122dd1e11a998
wangsp14 <wangsp14@midea.com>
2023-04-19 16:51:51 +0800

    通过串口发送设备或者模组指令.

--- include/cmdDevice.hpp
+++ include/cmdDevice.hpp
@@ -3,6 +3,8 @@
 #include "deviceOrderProcess.hpp"
 #include "wifiOrderProcess.hpp"
 #include <map>
+#include <string>
+#include <vector>
 extern DeviceOrder deviceOrder;
 extern WiFiOrder wifiOrder;
 class CmdDevice : public Commander
@@ -36,6 +38,95 @@ public:
     }
 
 private:
+    // 对字符串进行拆分
+    vector<string> split(const string &str, const string &delim)
+    {
+        vector<string> res;
+        if ("" == str)
+            return res;
+        // 先将要切割的字符串从string类型转换为char*类型
+        char *strs = new char[str.length() + 1]; // 不要忘了
+        strcpy(strs, str.c_str());
+
+        char *d = new char[delim.length() + 1];
+        strcpy(d, delim.c_str());
+
+        char *p = strtok(strs, d);
+        while (p)
+        {
+            string s = p;     // 分割得到的字符串转换为string类型
+            res.push_back(s); // 存入结果数组
+            p = strtok(NULL, d);
+        }
+
+        return res;
+    }
+    uint8_t strhex2u8(string str)
+    {
+        uint8_t num = 0;
+        int len = str.length();
+        for (int i = 0; i < len; ++i)
+        {
+            if (str[i] >= '0' && str[i] <= '9')
+            {
+                num += (str[i] - '0');
+            }
+            else if (str[i] >= 'a' && str[i] <= 'f')
+            {
+                num += (str[i] - 'a' + 10);
+            }
+            else if (str[i] >= 'A' && str[i] <= 'F')
+            {
+                num += (str[i] - 'A' + 10);
+            }
+            if (i < len - 1)
+            {
+                num *= 16;
+            }
+        }
+        return num;
+    }
+    /**
+     * 发送指令
+     */
+    uint8_t orderTemp;
+    void sendOrder(DynamicJsonDocument doc)
+    {
+        bool send2wifi = false;
+        if (!doc["module"].isNull())
+        {
+            if (strcmp(doc["module"], "wifi") == 0)
+            {
+                send2wifi = true;
+            }
+        }
+        if (!doc["order"].isNull())
+        {
+            const char *_order = doc["order"];
+            string order = _order;
+            vector<string> orderVec = split(order, " ");
+            if (orderVec.size() < 10)
+            {
+                orderVec = split(order, ",");
+            }
+            if (orderVec.size() >= 10)
+            {
+                for (int i = 0; i < orderVec.size(); ++i)
+                {
+                    orderTemp = strhex2u8(orderVec[i]);
+                    if (send2wifi)
+                    {
+                        HAL_UART_Transmit(&UartWiFiHandle, (uint8_t *)(&orderTemp), 1, 1000);
+                    }
+                    else
+                    {
+                        HAL_UART_Transmit(&UartDeviceHandle, (uint8_t *)(&orderTemp), 1, 1000);
+                    }
+                }
+                cout << ">> 发送指令成功." << endl;
+            }
+        }
+    }
     /**
      * 设备重启
      */
@@ -160,6 +251,7 @@ private:
         funcMap.insert(make_pair("queryMac", &CmdDevice::queryMac));
         funcMap.insert(make_pair("queryWiFiVersion", &CmdDevice::queryWiFiVersion));
         funcMap.insert(make_pair("queryNetState", &CmdDevice::queryNetState));
+        funcMap.insert(make_pair("sendOrder", &CmdDevice::sendOrder));
         funcDescMap.insert(make_pair("save", "保存设备变量,example: {\"cmd\":\"device\",\"op\":\"save\"}"));
         funcDescMap.insert(make_pair("set", "设置设备变量,example: {\"cmd\":\"device\",\"op\":\"set\",\"boolCheatA0\":true,\"projectNo\":13104,\"sn\":\"0000DB39188888888341800000130000\",\"boolCheatSN\":true,\"hotPrefix\":\"midea\"}"));
         funcDescMap.insert(make_pair("help", "帮助,example: {\"cmd\":\"device\",\"op\":\"help\"}"));
@@ -169,6 +261,7 @@ private:
         funcDescMap.insert(make_pair("queryMac", "获取 WiFi Mac,example: {\"cmd\":\"device\",\"op\":\"queryMac\"}"));
         funcDescMap.insert(make_pair("queryWiFiVersion", "获取 WiFi 版本,example: {\"cmd\":\"device\",\"op\":\"queryWiFiVersion\"}"));
         funcDescMap.insert(make_pair("queryNetState", "获取 WiFi 网络状态,example: {\"cmd\":\"device\",\"op\":\"queryNetState\"}"));
+        funcDescMap.insert(make_pair("sendOrder", "发送指令到设备/WiFi,example: {\"cmd\":\"device\",\"op\":\"sendOrder\",\"module\":\"wifi\",\"order\":\"AA 0B DB 00 00 00 00 00 00 03 03 14\"}"));
     }
     // 传入函数名，执行对应的函数
     void RunFunc(string funcName, DynamicJsonDocument doc)
